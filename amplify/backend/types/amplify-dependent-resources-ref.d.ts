f0bcbb5c0e2de7e36a54aa774b5550f370f5ef96
Tomo-ja <tomonao.hj@gmail.com>
2022-10-12 10:36:55 -0700

    set up amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,39 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "bookstoref0d1abf3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "bookImage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "processPayment": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "createOrder": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "bookstore": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
