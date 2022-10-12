10b774e67c810176ce121e0a759053de97922978
Tomo-ja <tomonao.hj@gmail.com>
2022-10-12 12:16:08 -0700

    set router within the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Amplify from 'aws-amplify';
+
+import Home from './pages/Home';
+import Books from './pages/Books';
+import BookDetails from './pages/BookDetails';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Admin from './pages/Admin';
+import Error from './pages/Error';
+
+import Header from './components/Header';
+
+import awsExports from './aws-exports'
+Amplify.configure(awsExports)
+
 
 function App() {
   return (
-    <div>
-      Hello, app
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<Books />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/books/:id' element={<BookDetails />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import StyledHeader from "./header.styles";
+
+
+
+const Header = () => {
+	return (
+		<StyledHeader>
+			<nav>
+				<h1>Book Store</h1>
+				<ul>
+					<li><Link to='/'>Home</Link></li>
+					<li><Link to='/books'>Books</Link></li>
+					<li><Link to='/cart'>Cart</Link></li>
+					<li><Link to='/checkout'>Checkout</Link></li>
+				</ul>
+			</nav>
+		</StyledHeader>
+	)
+}
+
+export default Header
