82f39ff3e7c88763097cae6502fe2c0447c9b01c
Tomo-ja <tomonao.hj@gmail.com>
2022-10-13 13:58:55 -0700

    create header

--- src/components/Header/header.styles.ts
+++ src/components/Header/header.styles.ts
@@ -2,6 +2,35 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
 
+	background-color: #131c27;
+	color: white;
+
+	nav{
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		width: 90%;
+		min-height: 10vh;
+		padding: 2rem;
+		margin: auto;
+	}
+
+	h1{
+		flex: 2 1 10rem;
+		font-weight: 400;
+	}
+
+	ul{
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		list-style: none;
+	}
+	
+	li{
+		margin-left: 1em;
+	}
+
 `
 
 export default StyledHeader
