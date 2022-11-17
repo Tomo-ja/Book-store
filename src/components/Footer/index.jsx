ae6844a45275aa3df42b16c85825f4f971650c57
Tomo-ja <tomonao.hj@gmail.com>
2022-11-16 17:02:00 -0800

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import StyledFooter from './footer.styles'
+
+
+const Footer = () => {
+	return (
+		<StyledFooter>
+			<p>Designed & Built by Tomonao</p>
+		</StyledFooter>
+	)
+}
+
+export default Footer
